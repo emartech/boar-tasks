09864746f101a20bd901282856e15eba4ab0e8c8
Viktor Somodi <somodi.viktor@gmail.com>
2015-04-08 21:20:41 +0200

    add angular mocks as default test module

--- tasks/config.js
+++ tasks/config.js
@@ -33,7 +33,7 @@ Config.client = {
     extensions: ['.js'],
     buildPattern: 'client/app/!(*.spec).js',
     testPattern: 'client/app/**/*.spec.js',
-    testModules: [],
+    testModules: ['node_modules/angular-mocks/angular-mocks.js'],
     watchPattern: 'client/app/**/*.js',
     viewPattern: 'client/app/views/**/*.jade',
     vendorPattern: 'client/vendors.js',
